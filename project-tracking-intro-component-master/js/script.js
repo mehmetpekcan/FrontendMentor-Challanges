649b03015d9f1ae3bb702f831af8ec91df09fafd
Mehmet Pekcan <pekcanmehmett@gmail.com>
2020-03-29 15:14:33 +0300

    some changes

--- project-tracking-intro-component-master/js/script.js
+++
@@ -1,9 +0,0 @@
-const toggle = document.querySelector(".hamburger")
-const navigationMenu = document.querySelector("nav")
-const closeIcon = document.querySelector(".closeIcon")
-
-toggle.addEventListener("click", () => {
-   navigationMenu.classList.toggle("showMenu")
-   toggle.childNodes[1].classList.toggle("changeIcon")
-   closeIcon.classList.toggle("showMenu")
-})
